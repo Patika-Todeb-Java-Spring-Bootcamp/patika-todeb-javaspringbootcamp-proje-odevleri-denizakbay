6fde459c1cdbae8cc01ff2dd51c7534a478bbd6a
deniz akbay <deniz.akbay843@gmail.com>
2022-08-07 23:00:03 +0300

    first commit

--- src/main/java/org/patika/advertproject/service/AdvertService.java
+++ src/main/java/org/patika/advertproject/service/AdvertService.java
@@ -51,4 +51,5 @@ public class AdvertService {
         }
        return advertRepository.save(advert);
     }
+
 }
---
+++ src/test/java/org/patika/advertproject/service/AdvertServiceTest.java
@@ -0,0 +1,28 @@
+package org.patika.advertproject.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdvertServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void createAdvert() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteAdvert() {
+    }
+
+    @Test
+    void update() {
+    }
+}
